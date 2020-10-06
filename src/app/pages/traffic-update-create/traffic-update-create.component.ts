0b6b68fc5c10065f00fd742f333e79096b0fb238
AsaVault <57707680+AsaVault@users.noreply.github.com>
2020-10-06 19:35:07 +0100

    traffic update create components modified

--- src/app/pages/traffic-update-create/traffic-update-create.component.ts
+++ src/app/pages/traffic-update-create/traffic-update-create.component.ts
@@ -1,4 +1,7 @@
+import { TrafficUpdateService } from 'src/app/services/traffic-update.service';
 import { Component, OnInit } from '@angular/core';
+import { TrafficUpdate } from 'src/app/models';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-traffic-update-create',
@@ -6,10 +9,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./traffic-update-create.component.scss']
 })
 export class TrafficUpdateCreateComponent implements OnInit {
+  trafficUpdate: TrafficUpdate;
+  title = '';
+  content = '';
+  constructor(private service: TrafficUpdateService, private route: Router) { }
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit(): void {
   }
 
+  submitTrafficUpdate(): void {
+    if (this.content.length === 0 || this.title.length === 0) {
+      return;
+    }
+    this.trafficUpdate = {
+      title: this.title,
+      description: this.content,
+      slug: 'nothing',
+      id: 0,
+      comments: null,
+      dateCreated: new Date(),
+    };
+    this.service
+      .postTrafficUpdate(this.trafficUpdate)
+      .subscribe((response) => {
+        this.content = '';
+        this.title = '';
+        this.route.navigate(['/traffic_updates']);
+      });
+  }
 }
