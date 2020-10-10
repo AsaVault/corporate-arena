07faf9b12b4c9f084646a085d6655ca0e7d7430f
AsaVault <57707680+AsaVault@users.noreply.github.com>
2020-10-10 17:27:47 +0100

    approve answer and display winner added to brain teaser service

--- src/app/services/brain-teaser.service.ts
+++ src/app/services/brain-teaser.service.ts
@@ -1,8 +1,10 @@
+import { BrainTeaserRequest } from './../models/BrainTeaserRequest';
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { BrainTeaser, Comment, CommentRequest } from '../models';
 import { Observable } from 'rxjs';
 import { ApiUrls } from '../app-config';
+import { BrainTeaserAnswer } from '../models/BrainTeaserAnswer';
 @Injectable({
   providedIn: 'root',
 })
@@ -19,9 +21,33 @@ export class BrainTeaserService {
     return this.http.get<BrainTeaser[]>(`${this.brainTeaserUrl}/GetAllBrainTeasers`);
   }
 
-  postComment(id: number, data: CommentRequest): Observable<Comment> {
-    return this.http.post<Comment>(
-      `${this.brainTeaserUrl}/${id}/comment`,
+  get(slug: string): Observable<BrainTeaser> {
+    return this.http.get<BrainTeaser>(`${this.brainTeaserUrl}/GetBrainTeaserAndWinner/${slug}`);
+  }
+
+  getWithAnswer(slug: string): Observable<BrainTeaser> {
+    return this.http.get<BrainTeaser>(`${this.brainTeaserUrl}/GetBrainTeaserAndAnswer/${slug}`);
+  }
+
+  postAnswer(id: number, data: BrainTeaserAnswer): Observable<BrainTeaserAnswer> {
+    return this.http.post<BrainTeaserAnswer>(
+      `${this.brainTeaserUrl}/AnswerBrainTeaser`,
+      data,
+      this.httpOptions
+    );
+  }
+
+  approveAnswer(data: BrainTeaserRequest): Observable<BrainTeaserRequest> {
+    return this.http.post<BrainTeaserRequest>(
+      `${this.brainTeaserUrl}/ApproveBrainTeaserAnswer`,
+      data,
+      this.httpOptions
+    );
+  }
+
+  displayWinner(data: BrainTeaserRequest): Observable<BrainTeaserRequest> {
+    return this.http.post<BrainTeaserRequest>(
+      `${this.brainTeaserUrl}/DisplayBrainTeaserWinner`,
       data,
       this.httpOptions
     );
