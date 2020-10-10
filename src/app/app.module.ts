6a6ab7cae767fe0145e4615a48c7b109a582da2a
AsaVault <57707680+AsaVault@users.noreply.github.com>
2020-10-10 18:11:02 +0100

    app module import rearranged

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,3 @@
-import { BlogCreateComponent } from './pages/blog-create/blog-create.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, Component } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
@@ -11,13 +10,17 @@ import { FooterComponent } from './component/footer/footer.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { BrainTeaserComponent } from './pages/brain-teaser/brain-teaser.component';
+import { BrainTeaserGetComponent } from './pages/brain-teaser-get/brain-teaser-get.component';
+import { BrainTeaserGetWithAnswerComponent } from './pages/brain-teaser-get-with-answer/brain-teaser-get-with-answer.component';
 import { NewsletterComponent } from './component/newsletter/newsletter.component';
 import { TrafficUpdateComponent } from './pages/traffic-update/traffic-update.component';
 import { TrafficUpdateArticleComponent } from './pages/traffic-update-article/traffic-update-article.component';
 import { BlogComponent } from './pages/blog/blog.component';
+import { BlogCreateComponent } from './pages/blog-create/blog-create.component';
 import { BlogArticleComponent } from './pages/blog-article/blog-article.component';
 import { VacanciesComponent } from './pages/vacancies/vacancies.component';
 import { TrafficUpdateCreateComponent } from './pages/traffic-update-create/traffic-update-create.component';
+
 
 @NgModule({
   declarations: [
@@ -27,6 +30,8 @@ import { TrafficUpdateCreateComponent } from './pages/traffic-update-create/traf
     HomeComponent,
     ContactComponent,
     BrainTeaserComponent,
+    BrainTeaserGetComponent,
+    BrainTeaserGetWithAnswerComponent,
     NewsletterComponent,
     TrafficUpdateComponent,
     TrafficUpdateArticleComponent,
