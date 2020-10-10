03562d08d320fe9f2a55aaa678aa9824e3965c4e
AsaVault <57707680+AsaVault@users.noreply.github.com>
2020-10-10 18:07:44 +0100

    brain teaser additional routing added to app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,5 @@
+import { BrainTeaserGetWithAnswerComponent } from './pages/brain-teaser-get-with-answer/brain-teaser-get-with-answer.component';
+import { BrainTeaserGetComponent } from './pages/brain-teaser-get/brain-teaser-get.component';
 import { TrafficUpdateCreateComponent } from './pages/traffic-update-create/traffic-update-create.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
@@ -15,6 +17,8 @@ const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'contact', component: ContactComponent },
   { path: 'brain_teasers', component: BrainTeaserComponent },
+  { path: 'brain-teaser/:id', component: BrainTeaserGetComponent },
+  { path: 'brain-teaser-admin/:id', component: BrainTeaserGetWithAnswerComponent },
   { path: 'vacancies', component: VacanciesComponent },
   { path: 'blog', component: BlogComponent },
   { path: 'blog-create', component: BlogCreateComponent },
