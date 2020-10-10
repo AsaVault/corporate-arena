a412c7380d6515ff8c7603935d415b843d01a651
AsaVault <57707680+AsaVault@users.noreply.github.com>
2020-10-10 16:40:04 +0100

    brainTeaserUrl added

--- src/app/services/brain-teaser.service.ts
+++ src/app/services/brain-teaser.service.ts
@@ -2,19 +2,21 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { BrainTeaser, Comment, CommentRequest } from '../models';
 import { Observable } from 'rxjs';
+import { ApiUrls } from '../app-config';
 @Injectable({
   providedIn: 'root',
 })
 export class BrainTeaserService {
-  brainTeaserUrl = 'https://localhost:44340/brainteaser';
+  private apiURL = ApiUrls.ApiURL;
+  brainTeaserUrl = `${this.apiURL}BrainTeaser`;
   httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
   };
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
   getAll(): Observable<BrainTeaser[]> {
-    return this.http.get<BrainTeaser[]>(this.brainTeaserUrl);
+    return this.http.get<BrainTeaser[]>(`${this.brainTeaserUrl}/GetAllBrainTeasers`);
   }
 
   postComment(id: number, data: CommentRequest): Observable<Comment> {
